5e1422b8373e98fc7c85735e9eb91415ba73fd01
Andrew <AND2181591@maricopa.edu>
2021-11-16 18:51:42 -0700

    Added axios and json server to the project. Also added fetch and save methods for the User class and extracted events from the class into its own to use composition.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
 import { User } from "./models/User";
 
-const user = new User({ name: 'myname', age: 20 });
-
-user.set({ age: 25 });
-
-console.log(user.get('name'));
-console.log(user.get('age'));
+const user = new User({ name: 'NEW RECORDS', age: 5656 });
